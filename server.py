f6472dbd0a1b9836af31ff198b4b3509203ce716
CONESTOGAC\rqian9700 <ruoyucad@gmail.com>
2017-11-25 16:43:53 -0500

    hosting locally

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from sqlalchemy import create_engine
 from json import dumps
 from flask.ext.jsonpify import jsonify
 
 db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
+cors = CORS(app, resorces={r'/d/*': {"origins": '*'}})
 api = Api(app)
+CORS(app, origins="http://127.0.0.1:5002", allow_headers=[
+    "Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+    supports_credentials=True)
+
 
 class Employees(Resource):
     def get(self):
@@ -27,12 +33,13 @@ class Employees_Name(Resource):
         query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
-        
+
+       
 
 api.add_resource(Employees, '/employees') # Route_1
 api.add_resource(Tracks, '/tracks') # Route_2
 api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 
-
+# following line will not run on pythonanywhere
 if __name__ == '__main__':
      app.run(port='5002')
