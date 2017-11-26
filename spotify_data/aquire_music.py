af9c23000e3801bef460caaef2b52c5dcf769b2c
Ruoyu Qian <ruoyucad@gmail.com>
2017-11-26 00:33:38 -0500

    adding rawdata to mysql (pymsql) database

--- spotify_data/aquire_music.py
+++ spotify_data/aquire_music.py
@@ -2,17 +2,38 @@ import pandas as pd
 import spotipy 
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotify_config import credential 
+from sqlalchemy import create_engine  
+import pymysql
 
+# get authorized by spotify
 sp = spotipy.Spotify() 
-
 cid = credential['clientID'] 
 secret = credential['clientSecrect']
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager) 
-
-
-BQB5wdUDAz5NCUu5WwEhBdprWGsAsiS_R9fVbgnRoczeqtf8FyrsSSzZDLi-wHC2u14otRXY1-FLQOGcGKcyllPNNX44kK7MecSM8SomvffOC93aqDjdB9xy_5Da6nWF_8CaiC7dELPOEOxcIWx-m0M-T99I5pJimqv37Du08px1MZ9LVs002t1ZNDUENxE3iA2aNu99_WIjRauGi9_Xp7lCTdTr8Ybmls3C08hPPrZj02EpkuHr3cU6ZD11tFaZ1BUzinqpt3c
 sp.trace=False 
 
-# playlist is chill vibes
-myplaylist = sp.user_playlist('bossangelo','37i9dQZF1DX889U0CL85jj' )
+# playlist contains 83 songs
+myplaylist = sp.user_playlist('bossangelo','5StZ5Iuq3puwrJKsg2Sdh0' )
+songs = myplaylist['tracks']['items']
+ids = []
+for i in range(len(songs)):
+    ids.append(songs[i]['track']['id'])
+    
+songNames = []
+for i in range(len(songs)):
+    songNames.append(songs[i]['track']['name'])
+
+# adding song attributes
+features = sp.audio_features(ids)
+musicdata = pd.DataFrame(features)
+musicdata['song_name'] = songNames
+musicdata['song_ids'] = ids
+print('Done !')
+
+#making database connection
+myconnection = create_engine('mysql+pymysql://angelo:!Qry778899@127.0.0.1/angelo?charset=utf8')
+print('db connected ...')
+print('writing data to database now... ')
+pd.io.sql.to_sql(musicdata,'mymusic', myconnection, schema='angelo', if_exists='append')  
+print('Complete !')
